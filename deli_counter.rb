1c42b7179d47b02f1d6fd705dac8d16461b3d3f0
Cameron J Leverett <cameron.leverett@yahoo.com>
2020-02-18 04:33:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line (katz_deli)
 else
   message = "The line is currently:"
   katz_deli.each_with_index do |name, number|
-   message += "#{number+1}. #{name}"
+   message += " #{number+1}. #{name}"
 end
 puts message
 end
