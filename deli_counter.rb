73436f327e717e84e00718244d69785159b6c14a
Cameron J Leverett <cameron.leverett@yahoo.com>
2020-02-18 02:49:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,7 +11,7 @@ end
 
 def take_a_number(katz_deli, name)
 katz_deli.push(name)
-puts "welcome, #{name}. You are number #{katz_deli.length} in"
+puts "welcome, #{name}. You are number #{katz_deli.length} in line."
 
 end
 
