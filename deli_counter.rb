a19f18d8a526387a29e241421a2eac38136cab3d
Cameron J Leverett <cameron.leverett@yahoo.com>
2020-02-18 02:27:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,8 @@ katz_deli = []
 def line (katz_deli)
   if katz_deli.length == 0
   puts "The line is currently empty."
+
 else puts "The line is currently:"
-els
 end
 
 def take_a_number(katz_deli, name)
