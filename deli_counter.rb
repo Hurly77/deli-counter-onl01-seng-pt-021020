feff8583ac0ccbb4737134d99fd743fafc572f30
Cameron J Leverett <cameron.leverett@yahoo.com>
2020-02-18 02:38:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line (katz_deli)
   if katz_deli.length == 0
   puts "The line is currently empty."
 
-elsif katz_deli.length < 0
+else katz_deli.length < 0
   katz_deli.each_index { |number, name|  }
    puts "The line is currently: #{number, name}"
 end
