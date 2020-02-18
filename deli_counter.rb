9ab78dd5a6311181ce632bfcfcee81b5787ceb97
Cameron J Leverett <cameron.leverett@yahoo.com>
2020-02-18 04:23:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line (katz_deli)
   puts "The line is currently empty."
 
 else
-  message = " "
+  message = "The line is currently:"
   katz_deli.each_with_index do |name, number|
    " #{number}. #{name}"
 end
